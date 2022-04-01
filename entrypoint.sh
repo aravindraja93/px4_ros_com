e3f153f3834942610fb400aa1760ec23283e070c
Jari Nippula <jarix@ssrc.tii.ae>
2022-04-01 12:03:33 +0300

    Add hypervisor address for protocol_splitter connection

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ if [ "${FLIGHT_CONTROLLER_DIRECT_ETH}" != "" ]; then
   exec micrortps_agent -t UDP -b 10000000 -i 192.168.200.101 -n "$DRONE_DEVICE_ID"
 else
   # FC connection via protocol_splitter/UART
-  exec micrortps_agent -t UDP -b 2000000 -n "$DRONE_DEVICE_ID"
+  exec micrortps_agent -t UDP -b 2000000 -i 192.168.200.100  -n "$DRONE_DEVICE_ID"
 fi
