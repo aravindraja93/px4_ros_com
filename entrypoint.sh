25439028a075a7a094fc23e02488398b4d9a1add
joonas.fi <joonas.loppi@unikie.com>
2022-04-07 10:46:18 +0300

    connect to FC via hypervisor port forward

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ fi
 
 if [ "${FLIGHT_CONTROLLER_DIRECT_ETH}" != "" ]; then
   # Direct ethernet connection to FC
-  exec micrortps_agent -t UDP -b 10000000 -i 192.168.200.101 -n "$DRONE_DEVICE_ID"
+  exec micrortps_agent -t UDP -b 10000000 -i 192.168.200.100 -n "$DRONE_DEVICE_ID"
 else
   # FC connection via protocol_splitter/UART
   exec micrortps_agent -t UDP -b 2000000 -n "$DRONE_DEVICE_ID"
